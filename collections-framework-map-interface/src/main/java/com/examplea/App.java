b59d8a4bd0ca136d873af48424789aed50fe36ac
Carolina <carol18becerra@gmail.com>
2026-05-05 14:53:12 +0200

    mapa creado a partir de operaciones de agregado

--- collections-framework-map-interface/src/main/java/com/examplea/App.java
+++ collections-framework-map-interface/src/main/java/com/examplea/App.java
@@ -1,9 +1,11 @@
 package com.examplea;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class App {
 	public static void main(String[] args) {
@@ -42,25 +44,10 @@ public class App {
 
 		/* Creacion del mapa a partir del listado de argumentos */
 
-		Map<String, Integer> m = new HashMap<>();
-
-		/*
-		 * Para agregar entradas al mapa m recorreremos la lista listadoDeArgumentos
-		 * ulizando una sentencia for mejorada
-		 */
-
-		Integer frecuenciaOcurrencia = null;
-
-		for (String nombre : listadoDeArgumentos) {
-			// comprobar si el nombre, clave ya se encuentra en el mapa m
-
-			frecuenciaOcurrencia = m.get(nombre);
-			m.put(nombre, frecuenciaOcurrencia == null
-					? 1
-					: ++ frecuenciaOcurrencia);
-		}
-		
-		System.out.println("Mapa resultante " + m);
+		Map<String, Long> m = listadoDeArgumentos.stream()
+				.collect(Collectors.groupingBy(nombre -> nombre, 
+						Collectors.counting()));
+		System.out.println("Mapa resultante: " + m);
 
 	}
 }
